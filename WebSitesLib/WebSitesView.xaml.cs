5f38191f6242e1e38da78195f4899e452cf604d3
Admin <Admin@LAPTOP-4HRM131V>
2025-05-19 23:50:40 +0300

    fixed hebrewBooks

---
+++ WebSitesLib/WebSitesView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Controls;
+
+namespace WebSitesLib
+{
+    /// <summary>
+    /// Interaction logic for WebSitesView.xaml
+    /// </summary>
+    public partial class WebSitesView : UserControl
+    {
+        public WebSitesView()
+        {
+            InitializeComponent();
+            string slectedIndex = Interaction.GetSetting(AppDomain.CurrentDomain.FriendlyName, "WebSitesView", "SelectedIndex", AdressBar.SelectedIndex.ToString());
+            if (int.TryParse(slectedIndex, out int index))
+                AdressBar.SelectedIndex = index;
+        }
+
+        private void AdressBar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Interaction.SaveSetting(AppDomain.CurrentDomain.FriendlyName, "WebSitesView", "SelectedIndex", AdressBar.SelectedIndex.ToString());
+        }
+
+        private void GobackButton_Click(object sender, System.Windows.RoutedEventArgs e) =>
+            browser?.GoBack();
+
+        private void GoForwardButton_Click(object sender, System.Windows.RoutedEventArgs e) =>
+            browser?.GoForward();
+
+        private void RefreshButton_Click(object sender, System.Windows.RoutedEventArgs e) =>
+            browser?.CoreWebView2?.Reload();
+    }
+}
---
+++ WebSitesLib/WebSitesViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using WpfLib;
+using WpfLib.ViewModels;
+
+namespace WebSitesLib
+{
+    public class WebSitesViewModel : ViewModelBase
+    {
+        string _jsonFileName = "WebSitesWhitelist.json";
+        ObservableCollection<WebAdressModel> _adresses;
+        public ObservableCollection<WebAdressModel> Adresses { get => _adresses; set => SetProperty(ref _adresses, value); }
+
+        public RelayCommand EditWhiteListCommand => new RelayCommand(() => ShowWhiteListDialog());
+
+        public WebSitesViewModel() 
+        {
+            PopulateAdressList();
+        }
+
+        void PopulateAdressList()
+        {
+            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", _jsonFileName);
+            if (File.Exists(jsonPath))
+            {
+                string json = File.ReadAllText(jsonPath);
+                Adresses = JsonSerializer.Deserialize<ObservableCollection<WebAdressModel>>(json) ?? new ObservableCollection<WebAdressModel>();
+            }
+        }
+
+        void ShowWhiteListDialog()
+        {
+            WhiteListDialog dialog = new WhiteListDialog(this);
+            dialog.ShowDialog();
+
+            string json = JsonSerializer.Serialize(Adresses);
+            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _jsonFileName);
+            if (File.Exists(jsonPath))
+                File.WriteAllText(jsonPath, json);
+        }
+    }
+}
