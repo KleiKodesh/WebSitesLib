920b4616af1bff3333a7d26182ecd157bba5f725
Admin <Admin@LAPTOP-4HRM131V>
2025-05-28 22:30:25 +0300

    updated theming and user settings

--- WebSitesLib/WebSitesViewModel.cs
+++ WebSitesLib/WebSitesViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using WpfLib;
 using WpfLib.ViewModels;
@@ -9,9 +11,18 @@ namespace WebSitesLib
 {
     public class WebSitesViewModel : ViewModelBase
     {
-        string _jsonFileName = "WebSitesWhitelist.json";
         ObservableCollection<WebAdressModel> _adresses;
-        public ObservableCollection<WebAdressModel> Adresses { get => _adresses; set => SetProperty(ref _adresses, value); }
+        string _mainFileName = "WebSitesWhitelist.json";
+        string _settingsFileName = "UserWebSitesSettings.json";
+
+        string AppDataFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string MainJsonPath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", _mainFileName);
+        string UserSettingsPath => Path.Combine(AppDataFolder, "WebWaysLib", _settingsFileName);  
+        public ObservableCollection<WebAdressModel> Adresses 
+        {
+            get => _adresses; 
+            set => SetProperty(ref _adresses, value); 
+        }
 
         public RelayCommand EditWhiteListCommand => new RelayCommand(() => ShowWhiteListDialog());
 
@@ -22,11 +33,22 @@ namespace WebSitesLib
 
         void PopulateAdressList()
         {
-            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", _jsonFileName);
-            if (File.Exists(jsonPath))
+            if (File.Exists(MainJsonPath))
             {
-                string json = File.ReadAllText(jsonPath);
-                Adresses = JsonSerializer.Deserialize<ObservableCollection<WebAdressModel>>(json) ?? new ObservableCollection<WebAdressModel>();
+                string json = File.ReadAllText(MainJsonPath);
+                Adresses = JsonSerializer.Deserialize<ObservableCollection<WebAdressModel>>(json)
+                    ?? new ObservableCollection<WebAdressModel>();
+            }
+
+            if (File.Exists(UserSettingsPath))
+            {
+                string userJson = File.ReadAllText(UserSettingsPath);
+                var userSettings = JsonSerializer.Deserialize<Dictionary<string, bool>>(userJson) ??
+                    new Dictionary<string, bool>();
+
+                foreach (var address in Adresses)
+                    if (userSettings.TryGetValue(address.Url, out bool isVisible))
+                        address.IsVisible = isVisible;
             }
         }
 
@@ -34,11 +56,22 @@ namespace WebSitesLib
         {
             WhiteListDialog dialog = new WhiteListDialog(this);
             dialog.ShowDialog();
-
-            string json = JsonSerializer.Serialize(Adresses);
-            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _jsonFileName);
-            if (File.Exists(jsonPath))
-                File.WriteAllText(jsonPath, json);
+            SaveUserSettings();
         }
+
+        void SaveUserSettings()
+        {
+            string directoryPath = Path.GetDirectoryName(UserSettingsPath);
+            if (!string.IsNullOrEmpty(directoryPath) && !Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            var userSettings = Adresses
+                .Where(address => address.IsVisible.HasValue)
+                .ToDictionary(address => address.Url, address => address.IsVisible.Value);
+
+            string userJson = JsonSerializer.Serialize(userSettings, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(UserSettingsPath, userJson);
+        }
+
     }
 }
