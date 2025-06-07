f87f18e761c0d9a86c6003ac81d350426b8ae894
Admin <Admin@LAPTOP-4HRM131V>
2025-06-08 00:52:20 +0300

    added last item history

--- WebSitesLib/WebSitesView.xaml.cs
+++ WebSitesLib/WebSitesView.xaml.cs
@@ -15,12 +15,22 @@ namespace WebSitesLib
             string selectedIndex = Interaction.GetSetting(AppDomain.CurrentDomain.FriendlyName, "WebSitesView", "SelectedIndex", AdressBar.SelectedIndex.ToString());
             if (int.TryParse(selectedIndex, out int index))
                 AdressBar.SelectedIndex = index;
+            browser.WebView.NavigationCompleted += WebView_NavigationCompleted;
+            browser.Loaded += Browser_Loaded;
         }
 
-        private void AdressBar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Browser_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
-            Interaction.SaveSetting(AppDomain.CurrentDomain.FriendlyName, "WebSitesView", "SelectedIndex", AdressBar.SelectedIndex.ToString());
+            string lastPage = Interaction.GetSetting(AppDomain.CurrentDomain.FriendlyName, "History", "LastPage", "https://kleikodesh.github.io/");
+            if (!string.IsNullOrEmpty(lastPage))
+                browser.Navigate(lastPage);
         }
+
+        private void WebView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) =>          
+                Interaction.SaveSetting( AppDomain.CurrentDomain.FriendlyName,  "History",   "LastPage", browser.WebView.Source.ToString());
+
+        private void AdressBar_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+            Interaction.SaveSetting(AppDomain.CurrentDomain.FriendlyName, "WebSitesView", "SelectedIndex", AdressBar.SelectedIndex.ToString());
 
         private void GobackButton_Click(object sender, System.Windows.RoutedEventArgs e) =>
             browser.WebView.GoBack();
